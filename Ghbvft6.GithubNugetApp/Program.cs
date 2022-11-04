a9f1e44bffaea1e7b1c615c6736e9f15fda0dbbd
greg-chuchro <gchuchro@acm.org>
2022-11-04 16:13:13 +0900

    init

---
+++ Ghbvft6.GithubNugetApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ghbvft6.GithubNugetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Ghbvft6.GithubNugetAppTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Ghbvft6.GithubNugetAppTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
